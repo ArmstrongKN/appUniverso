ac10e9aa84115762877bd8904923ff0415aaff8f
ArmstrongKN <kioshinambu@hotmail.com>
2024-05-20 22:42:25 -0300

    Correção da Flatlist

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Image, StyleSheet, Platform, FlatList, TouchableOpacity  } from 'react-native';
+import { Image, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
@@ -10,18 +10,17 @@ import * as ImagePicker from "expo-image-picker";
 
 export default function HomeScreen() {
 
-  const [image, setImage] = useState([]);
+    const [image, setImage] = useState("");
     const [files, setFiles] = useState([]);
 
     useEffect(() => {
-        const unsubscribe = onSnapshot(collection(fire, "files"), (snapshot) => {
+        const unsubscribe = onSnapshot(collection(fire, "universo"), (snapshot) => {
             snapshot.docChanges().forEach((change) => {
                 if (change.type === "added") {
                     setFiles((prevFiles) => [...prevFiles, change.doc.data()]);
                 }
             });
         });
-
         return () => unsubscribe();
     }, []);
 
@@ -47,16 +46,15 @@ export default function HomeScreen() {
 
     async function saveRecord(fileType, url, createdAt) {
         try {
-            await addDoc(collection(fire, "files"), {
+            const docRef = await addDoc(collection(fire, "universo"), {
                 fileType,
                 url,
                 createdAt,
-            });
+            })
         } catch (e) {
             console.log(e);
         }
     }
-
     async function pickImage() {
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -64,84 +62,84 @@ export default function HomeScreen() {
             aspect: [4, 3],
             quality: 1,
         });
-
+        console.log(result);
         if (!result.canceled) {
-            const { uri } = result.assets[0];
-            setImage(uri);
-            await uploadImage(uri, "image");
+            setImage(result.assets[0].uri);
+            await uploadImage(result.assets[0].uri, "img");
         }
-    }
-
+    };
     return (
 
-      <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/retratos/glx.png')}
-          style={styles.reactLogo}
-        />
-      }>
-
-        <ThemedView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 8, height: '100%', width: '100%' }}>
-
-        <ThemedText type="title"> Bem-Vindo </ThemedText>
-                <ThemedText type="defaultSemiBold" style={{ textAlign: 'center' }}>
-                    Arquivos Enviados
-                </ThemedText>
-                <FlatList
-                    data={files}
-                    keyExtractor={(item) => item.url}
-                    renderItem={({ item }) => {
-                        if (item.fileType === "image") {
-                            return (
-                                <Image
-                                    source={{ uri: item.url }}
-                                    style={{ width: 150, height: 150, borderRadius: 20, margin: 5 }}
-                                />
-                            );
-                        }
-                        return null;
-                    }}
-                    numColumns={1}
+        <ParallaxScrollView
+            headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+            headerImage={
+                <Image
+                    source={require('@/assets/images/retratos/glx.png')}
+                    style={styles.reactLogo}
                 />
-                <TouchableOpacity
-                    onPress={pickImage}
-                    style={{ justifyContent: 'center', alignItems: 'center', borderRadius: 20, padding: 50, backgroundColor: 'lightblue', marginTop: 10 }}
-                >
-                    <ThemedText>Selecionar Imagens</ThemedText>
-                </TouchableOpacity>
-        </ThemedView>
+            }>
+
+            <ThemedView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 8, height: '100%', width: '100%' }}>
+
+                <ThemedText type="title"> Bem-Vindo </ThemedText>
+                <ThemedText type="defaultSemiBold" style={{ textAlign: 'center' }}>
+                    Arquivos Enviado
+                </ThemedText>
+            </ThemedView>
+            <FlatList
+                data={files}
+                keyExtractor={(item) => item.url}
+                renderItem={({ item }) => {
+                    if (item.fileType === "img") {
+                        return (
+                            <Image
+                                source={{ uri: item.url }}
+                                style={{ width: 150, height: 150, borderRadius: 20, margin: 5 }}
+                            />
+                        )
+                    }
+                }
+                }
+                numColumns={2}
+
+            />
+            <TouchableOpacity
+                onPress={pickImage}
+                style={{ justifyContent: 'center', alignItems: 'center', borderRadius: 20, padding: 50, backgroundColor: 'lightblue', marginTop: 10 }}
+            >
+                <ThemedText>Selecionar Imagens</ThemedText>
+            </TouchableOpacity>
+
 
         </ParallaxScrollView>
     );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 250,
-    width: 415,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-  fotos:{
-      width: 200,
-      height: 200
-  },
-  imgpick:{
-    position: "absolute",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 20
-  }
+    titleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+    },
+    stepContainer: {
+        gap: 8,
+        marginBottom: 8,
+    },
+    reactLogo: {
+        height: 250,
+        width: 415,
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+    },
+    fotos: {
+        width: 200,
+        height: 200
+    },
+    imgpick: {
+        position: "absolute",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 20
+    }
 });
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,13 +3,22 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDIJegZ5yDvaDl4xwLRC9P2MVAo0IJ5p6s",
-  authDomain: "universo-121ee.firebaseapp.com",
-  projectId: "universo-121ee",
-  storageBucket: "universo-121ee.appspot.com",
-  messagingSenderId: "490412131995",
-  appId: "1:490412131995:web:8baf82fe67fa8ff09ae83e"
-  };
+  apiKey: "AIzaSyBGAmHDvlS1QqHh7U3ztuh2gHoFAA8Xhi4",
+  authDomain: "universo-ba8bb.firebaseapp.com",
+  projectId: "universo-ba8bb",
+  storageBucket: "universo-ba8bb.appspot.com",
+  messagingSenderId: "370296424532",
+  appId: "1:370296424532:web:7aae30fd233d1353b405c1"
+};
+
+
+// apiKey: "AIzaSyDIJegZ5yDvaDl4xwLRC9P2MVAo0IJ5p6s",
+//   authDomain: "universo-121ee.firebaseapp.com",
+//   projectId: "universo-121ee",
+//   storageBucket: "universo-121ee.appspot.com",
+//   messagingSenderId: "490412131995",
+//   appId: "1:490412131995:web:8baf82fe67fa8ff09ae83e"
+
   
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
